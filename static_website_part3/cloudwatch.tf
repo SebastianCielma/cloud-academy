026dd31aae513f5d29fa19404906b6b5451a4728
sebastiancielma <cielmasebastian@gmail.com>
2026-02-13 11:37:03 +0100

    feat: add cloudwatch

---
+++ static_website_part3/cloudwatch.tf
@@ -0,0 +1,60 @@
+resource "aws_cloudwatch_dashboard" "main" {
+  dashboard_name = "StaticWebsite-Monitor-Part3"
+  
+  dashboard_body = jsonencode({
+    widgets = [
+      {
+        type   = "metric"
+        x      = 0
+        y      = 0
+        width  = 12
+        height = 6
+        properties = {
+          metrics = [
+            [ "AWS/CloudFront", "Requests", "DistributionId", module.cloudfront.distribution_id, { "region": "us-east-1" } ]
+          ]
+          view    = "timeSeries"
+          stacked = false
+          region  = "us-east-1" 
+          title   = "Total Requests (Global)"
+          period  = 300
+        }
+      },
+      {
+        type   = "metric"
+        x      = 12
+        y      = 0
+        width  = 12
+        height = 6
+        properties = {
+          metrics = [
+            [ "AWS/CloudFront", "4xxErrorRate", "DistributionId", module.cloudfront.distribution_id, { "region": "us-east-1" } ],
+            [ "AWS/CloudFront", "5xxErrorRate", "DistributionId", module.cloudfront.distribution_id, { "region": "us-east-1" } ]
+          ]
+          view    = "timeSeries"
+          stacked = false
+          region  = "us-east-1"
+          title   = "Error Rates (4xx & 5xx)"
+          period  = 300
+        }
+      },
+      {
+        type   = "metric"
+        x      = 0
+        y      = 6
+        width  = 24
+        height = 6
+        properties = {
+          metrics = [
+            [ "AWS/WAFV2", "BlockedRequests", "WebACL", "cloudfront-rate-limit", "Region", "us-east-1", "Rule", "ALL", { "region": "us-east-1" } ]
+          ]
+          view    = "timeSeries"
+          stacked = false
+          region  = "us-east-1"
+          title   = "WAF Blocked Requests"
+          period  = 300
+        }
+      }
+    ]
+  })
+}
--- static_website_part3/modules/cloudfront/outputs.tf
+++ static_website_part3/modules/cloudfront/outputs.tf
@@ -5,3 +5,7 @@ output "distribution_arn" {
 output "domain_name" {
   value = aws_cloudfront_distribution.this.domain_name
 }
+
+output "distribution_id" {
+  value       = aws_cloudfront_distribution.this.id
+}

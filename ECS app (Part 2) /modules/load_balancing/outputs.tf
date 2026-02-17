289bba889f50318612c4a82160197c248f9aad79
sebastiancielma <cielmasebastian@gmail.com>
2026-02-17 14:11:37 +0100

    feat: add load balancing module configuration

---
+++ ECS app (Part 2) /modules/load_balancing/outputs.tf
@@ -0,0 +1,3 @@
+output "target_group_arn" { value = aws_lb_target_group.main.arn }
+output "alb_security_group_id" { value = aws_security_group.alb.id }
+output "alb_dns_name" { value = aws_lb.main.dns_name }

b98029f8dddd53ae38e758cdffbb4f4fa5ba6833
sebastiancielma <cielmasebastian@gmail.com>
2026-02-18 22:16:58 +0100

    feat: add scale in policy and low cpu alarm

--- ECS app (Part 3)/modules/compute/autoscaling.tf
+++ ECS app (Part 3)/modules/compute/autoscaling.tf
@@ -42,3 +42,40 @@ resource "aws_cloudwatch_metric_alarm" "high_cpu" {
 
   alarm_actions = [aws_appautoscaling_policy.scale_out.arn]
 }
+
+resource "aws_appautoscaling_policy" "scale_in" {
+  name               = "${var.project_name}-scale-in"
+  policy_type        = "StepScaling"
+  resource_id        = aws_appautoscaling_target.ecs_target.resource_id
+  scalable_dimension = aws_appautoscaling_target.ecs_target.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.ecs_target.service_namespace
+
+  step_scaling_policy_configuration {
+    adjustment_type         = "ChangeInCapacity"
+    cooldown                = 300
+    metric_aggregation_type = "Average"
+
+    step_adjustment {
+      metric_interval_upper_bound = 0
+      scaling_adjustment          = -1 
+    }
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "low_cpu" {
+  alarm_name          = "${var.project_name}-low-cpu"
+  comparison_operator = "LessThanThreshold"
+  evaluation_periods  = "5"   
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/ECS"
+  period              = "60"  
+  statistic           = "Average"
+  threshold           = "25"  
+
+  dimensions = {
+    ClusterName = aws_ecs_cluster.main.name
+    ServiceName = aws_ecs_service.main.name
+  }
+
+  alarm_actions = [aws_appautoscaling_policy.scale_in.arn]
+}

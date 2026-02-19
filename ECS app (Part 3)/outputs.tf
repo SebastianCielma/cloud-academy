87593bee24218ee424d969d7e2f6188a3d3ebdcd
sebastiancielma <cielmasebastian@gmail.com>
2026-02-19 13:45:23 +0100

    feat: add arn outputs

--- ECS app (Part 3)/outputs.tf
+++ ECS app (Part 3)/outputs.tf
@@ -5,3 +5,11 @@ output "alb_dns_name" {
 output "ecr_repository_url" {
   value = module.compute.ecr_repository_url
 }
+
+output "github_plan_role_arn" {
+   value = module.cicd.plan_role_arn 
+}
+
+output "github_apply_role_arn" { 
+  value = module.cicd.apply_role_arn
+}

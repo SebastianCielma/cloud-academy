14641e50e1df958834c3214e25d81f69d07d27f8
sebastiancielma <cielmasebastian@gmail.com>
2026-02-19 17:03:39 +0100

    fix: delete variable

--- ECS app (Part 3)/variables.tf
+++ ECS app (Part 3)/variables.tf
@@ -14,9 +14,3 @@ variable "vpc_cidr" {
   type        = string
   default     = "10.0.0.0/16"
 }
-
-variable "environment" {
-  description = "Deployment environment"
-  type        = string
-  default     = "dev"
-}

b6af7ba19ec2f1dffe8203b8555746b9c9244484
sebastiancielma <cielmasebastian@gmail.com>
2026-02-19 13:20:15 +0100

    feat: add cicd module main configuration

---
+++ ECS app (Part 3)/modules/cicd/main.tf
@@ -0,0 +1,69 @@
+data "tls_certificate" "github" {
+  url = "https://token.actions.githubusercontent.com/.well-known/openid-configuration"
+}
+
+resource "aws_iam_openid_connect_provider" "github" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.github.certificates[0].sha1_fingerprint]
+}
+
+data "aws_iam_policy_document" "github_assume_role" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.github.arn]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = ["repo:${var.github_repo}:*"] 
+    }
+  }
+}
+
+resource "aws_iam_role" "plan_role" {
+  name               = "${var.project_name}-github-plan-role"
+  assume_role_policy = data.aws_iam_policy_document.github_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "plan_read_only" {
+  role       = aws_iam_role.plan_role.name
+  policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
+}
+
+resource "aws_iam_role_policy" "plan_state_access" {
+  name   = "TerraformStateAccess"
+  role   = aws_iam_role.plan_role.id
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = ["s3:GetObject", "s3:PutObject", "s3:ListBucket"],
+        Resource = "*" 
+      },
+      {
+        Effect   = "Allow",
+        Action   = ["dynamodb:GetItem", "dynamodb:PutItem", "dynamodb:DeleteItem"],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role" "apply_role" {
+  name               = "${var.project_name}-github-apply-role"
+  assume_role_policy = data.aws_iam_policy_document.github_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "apply_admin" {
+  role       = aws_iam_role.apply_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}

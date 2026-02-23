bca1844f8e1cdad28ccef2516a3e8f3e26f617f5
sebastiancielma <cielmasebastian@gmail.com>
2026-02-23 15:22:30 +0100

    fix: add missing ingress rules to ALB and ECS security groups

--- ECS_troubleshoot/10_security_groups.tf
+++ ECS_troubleshoot/10_security_groups.tf
@@ -6,6 +6,12 @@ resource "aws_security_group" "alb" {
   description = "ALB security group"
   vpc_id      = aws_vpc.main.id
 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port   = 0
@@ -25,6 +31,12 @@ resource "aws_security_group" "tasks" {
   description = "ECS tasks security group"
   vpc_id      = aws_vpc.main.id
 
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb.id]
+  }
 
   egress {
     from_port   = 0

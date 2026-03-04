7798e539ea3f242addcf4cc474fe0baa7bf595b0
sebastiancielma <cielmasebastian@gmail.com>
2026-03-04 12:15:07 +0100

    feat: add modules variables

--- Cloud-cluster-provisioning-EKS-Part3/variables.tf
+++ Cloud-cluster-provisioning-EKS-Part3/variables.tf
@@ -3,3 +3,33 @@ variable "aws_region" {
   type        = string
   default     = "eu-central-1"
 }
+
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+}
+
+variable "vpc_name" {
+  description = "Name of the VPC"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "azs" {
+  description = "Availability zones for the subnets"
+  type        = list(string)
+}
+
+variable "private_subnets" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+}
+
+variable "public_subnets" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+}

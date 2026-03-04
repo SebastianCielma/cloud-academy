2fa53fc4ba5561cea05697b40b50ce68a29c90ca
sebastiancielma <cielmasebastian@gmail.com>
2026-03-04 11:48:53 +0100

    feat: add vpc module configuration

---
+++ Cloud-cluster-provisioning-EKS-Part3/modules/vpc/variables.tf
@@ -0,0 +1,24 @@
+variable "vpc_name" {
+  description = "The name of the VPC"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC"
+  type        = string
+}
+
+variable "azs" {
+  description = "A list of availability zones names or ids in the region"
+  type        = list(string)
+}
+
+variable "private_subnets" {
+  description = "A list of private subnets inside the VPC"
+  type        = list(string)
+}
+
+variable "public_subnets" {
+  description = "A list of public subnets inside the VPC"
+  type        = list(string)
+}

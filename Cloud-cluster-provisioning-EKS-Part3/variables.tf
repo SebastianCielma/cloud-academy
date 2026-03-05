14e3d1f31bb87b7f8e76dc3bedd7f549917b31db
sebastiancielma <cielmasebastian@gmail.com>
2026-03-05 13:35:48 +0100

    fix: add default values to variables.tf

--- Cloud-cluster-provisioning-EKS-Part3/variables.tf
+++ Cloud-cluster-provisioning-EKS-Part3/variables.tf
@@ -7,29 +7,35 @@ variable "aws_region" {
 variable "cluster_name" {
   description = "Name of the EKS cluster"
   type        = string
+  default     = "eks-private-cluster-part3"
 }
 
 variable "vpc_name" {
   description = "Name of the VPC"
   type        = string
+  default     = "eks-vpc-part3"
 }
 
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
+  default     = "10.0.0.0/16"
 }
 
 variable "azs" {
   description = "Availability zones for the subnets"
   type        = list(string)
+  default     = ["eu-central-1a", "eu-central-1b"]
 }
 
 variable "private_subnets" {
   description = "CIDR blocks for private subnets"
   type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "public_subnets" {
   description = "CIDR blocks for public subnets"
   type        = list(string)
+  default     = ["10.0.101.0/24", "10.0.102.0/24"]
 }

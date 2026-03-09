a1a904464a6ab3b7c6df221c4131fc8413a9c5f5
sebastiancielma <cielmasebastian@gmail.com>
2026-03-09 18:30:06 +0100

    feat: add vpc module configuration

---
+++ k8s_payment_app_part_0/terraform/modules/vpc/outputs.tf
@@ -0,0 +1,19 @@
+output "vpc_id" {
+  description = "The ID of the created VPC"
+  value       = module.vpc.vpc_id
+}
+
+output "private_subnets" {
+  description = "List of IDs of private subnets"
+  value       = module.vpc.private_subnets
+}
+
+output "public_subnets" {
+  description = "List of IDs of public subnets"
+  value       = module.vpc.public_subnets
+}
+
+output "azs" {
+  description = "List of Availability Zones used in the VPC"
+  value       = module.vpc.azs
+}

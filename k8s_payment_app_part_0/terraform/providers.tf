2e2cb7e1b9411fbbef85f62ec92254b9ee908fa4
sebastiancielma <cielmasebastian@gmail.com>
2026-03-10 14:49:33 +0100

    feat: add s3 bucket and dynamodb table for terraform state

---
+++ k8s_payment_app_part_0/terraform/providers.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  backend "s3" {
+    bucket         = "finpay-tf-state-twoje-imie" 
+    key            = "finpay/production/terraform.tfstate"
+    region         = "eu-central-1"
+    dynamodb_table = "finpay-tf-lock"
+    encrypt        = true
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-central-1"
+
+  default_tags {
+    tags = {
+      Project     = "FinPay"
+      Environment = "Production"
+      ManagedBy   = "Terraform"
+    }
+  }
+}

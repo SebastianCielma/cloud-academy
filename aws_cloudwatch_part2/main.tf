399bc3512cea68780968381e702edf5da01505e2
sebastiancielma <cielmasebastian@gmail.com>
2026-04-01 18:21:30 +0200

    feat: add root main configuration

---
+++ aws_cloudwatch_part2/main.tf
@@ -0,0 +1,71 @@
+# ------------------------------------------------------------------------
+# IAM EC2
+# ------------------------------------------------------------------------
+data "aws_iam_policy_document" "ec2_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "ec2_monitoring_role" {
+  name               = "ec2-cloudwatch-ssm-role"
+  assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "cw_agent" {
+  role       = aws_iam_role.ec2_monitoring_role.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_core" {
+  role       = aws_iam_role.ec2_monitoring_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "ec2-cloudwatch-ssm-profile"
+  role = aws_iam_role.ec2_monitoring_role.name
+}
+
+# ------------------------------------------------------------------------
+#  EC2 
+# ------------------------------------------------------------------------
+module "ec2_web" {
+  source               = "./modules/ec2_instance"
+  ami_id               = var.ami_id
+  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
+  
+  instance_name = "web-1"
+  env           = "prod"
+  app           = "web"
+  role          = "frontend"
+  auto_alert    = "true"
+}
+
+module "ec2_api" {
+  source               = "./modules/ec2_instance"
+  ami_id               = var.ami_id
+  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
+  
+  instance_name = "api-1"
+  env           = "prod"
+  app           = "api"
+  role          = "backend"
+  auto_alert    = "true"
+}
+
+module "ec2_worker" {
+  source               = "./modules/ec2_instance"
+  ami_id               = var.ami_id
+  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
+  
+  instance_name = "worker-1"
+  env           = "prod"
+  app           = "worker"
+  role          = "processing"
+  auto_alert    = "true"
+}

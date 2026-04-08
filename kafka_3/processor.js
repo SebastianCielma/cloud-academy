89f7a9eb8fb9a488ba655a1e4801b539ce6261c1
sebastiancielma <cielmasebastian@gmail.com>
2026-04-08 15:31:25 +0200

    feat: add processor configuration

---
+++ kafka_3/processor.js
@@ -0,0 +1,98 @@
+const { Kafka, Partitioners } = require('kafkajs');
+const { randomUUID } = require('crypto');
+
+const kafka = new Kafka({
+  clientId: 'processor-app',
+  brokers: ['localhost:9092']
+});
+
+const consumer = kafka.consumer({ groupId: 'processor-group' });
+const producer = kafka.producer({
+  createPartitioner: Partitioners.LegacyPartitioner
+});
+
+const sendToDLQ = async (originalMessage, reason, originalId = 'UNKNOWN') => {
+  const dlqEvent = {
+    dlq_event_id: randomUUID(),
+    original_event_id: originalId,
+    failed_at: new Date().toISOString(),
+    failure_reason: reason,
+    raw_payload: originalMessage
+  };
+
+  await producer.send({
+    topic: 'dead-letter-topic',
+    messages: [
+      { key: originalId, value: JSON.stringify(dlqEvent) }
+    ]
+  });
+  console.log(`Event ${originalId} sent to DLQ. Reason: ${reason}`);
+};
+
+const run = async () => {
+  await producer.connect();
+  await consumer.connect();
+  
+  console.log('Processor connected to Kafka. Starting to consume...');
+
+  await consumer.subscribe({ topic: 'raw-data-topic', fromBeginning: true });
+
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      const rawValue = message.value.toString();
+      let parsedEvent;
+
+      try {
+        parsedEvent = JSON.parse(rawValue);
+      } catch (error) {
+        await sendToDLQ(rawValue, 'INVALID_JSON_FORMAT');
+        return; 
+      }
+
+      const eventId = parsedEvent.event_id || 'UNKNOWN';
+      const amount = parsedEvent.payload?.amount;
+
+
+      if (amount === undefined || typeof amount !== 'number') {
+        await sendToDLQ(parsedEvent, 'INVALID_OR_MISSING_AMOUNT', eventId);
+        return; 
+      }
+
+
+      const processedEvent = {
+        processed_event_id: randomUUID(),       
+        original_event_id: eventId,             
+        status: 'PROCESSED_SUCCESSFULLY',
+        processed_at: new Date().toISOString(),
+        enriched_data: {
+          order_id: parsedEvent.payload.order_id,
+          customer_id: parsedEvent.payload.customer_id,
+          original_amount: amount,
+          tax_added: parseFloat((amount * 0.23).toFixed(2)), 
+          total_amount: parseFloat((amount * 1.23).toFixed(2))
+        }
+      };
+
+      await producer.send({
+        topic: 'processed-data-topic',
+        messages: [
+          { 
+            key: processedEvent.enriched_data.order_id, 
+            value: JSON.stringify(processedEvent) 
+          }
+        ]
+      });
+
+      console.log(`Event ${eventId} successfully transformed and routed.`);
+    },
+  });
+};
+
+run().catch(console.error);
+
+process.on('SIGINT', async () => {
+  console.log('\nClosing Processor connections');
+  await consumer.disconnect();
+  await producer.disconnect();
+  process.exit(0);
+});

232fe61c875c7fd341a4020fc793dee2d0bbba1f
sebastiancielma <cielmasebastian@gmail.com>
2026-04-17 12:41:42 +0200

    feat: add root outputs

---
+++ Jenkins_part4/platform-jenkins/outputs.tf
@@ -0,0 +1,9 @@
+output "jenkins_url" {
+  description = "The URL to access the Jenkins UI via the Application Load Balancer"
+  value       = "http://${aws_lb.jenkins_alb.dns_name}"
+}
+
+output "jenkins_private_ip" {
+  description = "Private IP address of the Jenkins EC2 instance (for SSM access)"
+  value       = module.jenkins_node.private_ip
+}

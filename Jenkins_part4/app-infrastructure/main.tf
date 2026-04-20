d1e4d2f36664b76dbc826ec40a1149f2c1ec4c77
sebastiancielma <cielmasebastian@gmail.com>
2026-04-20 14:42:13 +0200

    feat: add root main configuration

---
+++ Jenkins_part4/app-infrastructure/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  backend "s3" {
+    bucket         = "jenkins-part4-tf-state" 
+    key            = "app/terraform.tfstate"
+    region         = "eu-central-1"
+    dynamodb_table = "terraform-state-lock"
+  }
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
+
+locals {
+  env = terraform.workspace == "default" ? "dev" : terraform.workspace
+}
+
+module "ecr" {
+  source = "./modules/ecr"
+  env    = local.env
+}
+
+module "ecs" {
+  source = "./modules/ecs"
+  env    = local.env
+}
+
+output "ecr_repository_url" {
+  value = module.ecr.repository_url
+}

992ecd4aeab763400b2fc6c32c3883ed1e569660
sebastiancielma <cielmasebastian@gmail.com>
2026-05-08 17:36:10 +0200

    feat: add root variables

---
+++ azure_static_website_part2/variables.tf
@@ -0,0 +1,11 @@
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "polandcentral"
+}
+
+variable "project_name" {
+  description = "Main project name for resources"
+  type        = string
+  default     = "part2"
+}

69cc3fb3777edfb524a5773abc5f459dd6934f1a
sebastiancielma <cielmasebastian@gmail.com>
2026-05-18 13:06:53 +0200

    test cicd

--- azure-aci-app-part3/modules/log_analytics/main.tf
+++ azure-aci-app-part3/modules/log_analytics/main.tf
@@ -3,6 +3,6 @@ resource "azurerm_log_analytics_workspace" "law" {
   location            = var.location
   resource_group_name = var.resource_group_name
   sku                 = "PerGB2018"
-  retention_in_days   = 14
+  retention_in_days   = 30
   tags                = var.common_tags
 }

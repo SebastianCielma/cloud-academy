85ca02f5e2989b8c9c44702d5cbc2ecb85f2cff9
sebastiancielma <cielmasebastian@gmail.com>
2026-05-20 21:09:57 +0200

    test cicd

--- Cloud-cluster-provisioning-aks-part2/providers.tf
+++ Cloud-cluster-provisioning-aks-part2/providers.tf
@@ -1,19 +1,3 @@
-terraform {
-  required_version = ">= 1.5.0"
-  backend "azurerm" {
-    resource_group_name  = "rg-terraform-state"
-    storage_account_name = "tfstate2234816542"
-    container_name       = "tfstate"
-    key                  = "aks.terraform.tfstate"
-  }
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-  }
-}
-
 provider "azurerm" {
   features {}
 }

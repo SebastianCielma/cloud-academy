f771aab361300c3994c7d126b1221ff00578a715
sebastiancielma <cielmasebastian@gmail.com>
2026-05-21 23:52:45 +0200

    fix: network profile

--- Cloud-cluster-provisioning-aks-part3/modules/aks/main.tf
+++ Cloud-cluster-provisioning-aks-part3/modules/aks/main.tf
@@ -14,10 +14,13 @@ resource "azurerm_kubernetes_cluster" "aks" {
     vnet_subnet_id = var.aks_system_subnet_id
   }
 
-  network_profile {
+network_profile {
     network_plugin = "azure"
     network_policy = "azure"
     outbound_type  = "userAssignedNATGateway"
+    
+    service_cidr   = "10.1.0.0/16"
+    dns_service_ip = "10.1.0.10"
   }
 
   identity {

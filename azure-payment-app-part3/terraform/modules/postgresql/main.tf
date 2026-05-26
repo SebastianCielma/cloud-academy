3ce6b56ee49f2a9c6cdfb7ad1a0dfd9d1a82a2b1
sebastiancielma <cielmasebastian@gmail.com>
2026-05-26 21:22:06 +0200

    feat: add postgresql config

---
+++ azure-payment-app-part3/terraform/modules/postgresql/main.tf
@@ -0,0 +1,41 @@
+resource "azurerm_private_dns_zone" "postgresql" {
+  name                = "privatelink.postgres.database.azure.com"
+  resource_group_name = var.resource_group_name
+  tags                = var.tags
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "postgresql" {
+  name                  = "vnet-link-postgresql"
+  private_dns_zone_name = azurerm_private_dns_zone.postgresql.name
+  resource_group_name   = var.resource_group_name
+  virtual_network_id    = var.vnet_id
+}
+
+resource "azurerm_postgresql_flexible_server" "this" {
+  name                   = "psql-${var.project_name}-${var.environment}"
+  resource_group_name    = var.resource_group_name
+  location               = var.location
+  version                = var.postgresql_version
+  delegated_subnet_id    = var.database_subnet_id
+  private_dns_zone_id    = azurerm_private_dns_zone.postgresql.id
+  administrator_login    = var.admin_username
+  administrator_password = var.admin_password
+  zone                   = "1"
+  storage_mb             = var.storage_mb
+  sku_name               = var.sku_name
+  tags                   = var.tags
+
+  high_availability {
+    mode                      = "ZoneRedundant"
+    standby_availability_zone = "2"
+  }
+
+  depends_on = [azurerm_private_dns_zone_virtual_network_link.postgresql]
+}
+
+resource "azurerm_postgresql_flexible_server_database" "payment" {
+  name      = var.database_name
+  server_id = azurerm_postgresql_flexible_server.this.id
+  charset   = "UTF8"
+  collation = "en_US.utf8"
+}

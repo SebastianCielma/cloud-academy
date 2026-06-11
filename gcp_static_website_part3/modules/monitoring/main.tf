08ea83ecbd2dfa005a41460f18134f691e919651
sebastiancielma <cielmasebastian@gmail.com>
2026-06-11 10:41:40 +0200

    fix: tflint

--- gcp_static_website_part3/modules/monitoring/main.tf
+++ gcp_static_website_part3/modules/monitoring/main.tf
@@ -10,12 +10,16 @@ resource "google_monitoring_alert_policy" "armor_blocks" {
   display_name = "${var.prefix}-high-armor-blocks"
   combiner     = "OR"
   conditions {
-    display_name = "Cloud Armor Deny > 10 in 1 min"
+    display_name = "Cloud Armor Deny (403) > 10 in 1 min"
     condition_threshold {
-      filter          = "metric.type=\"networksecurity.googleapis.com/edge_security_policy/request_count\" resource.type=\"edge_security_policy\" metric.labels.outcome=\"DENY\""
+      filter          = "metric.type=\"loadbalancing.googleapis.com/https/request_count\" resource.type=\"https_lb_rule\" metric.labels.response_code_class=\"400\""
       duration        = "60s"
       comparison      = "COMPARISON_GT"
       threshold_value = 10
+      aggregations {
+        alignment_period   = "60s"
+        per_series_aligner = "ALIGN_RATE"
+      }
     }
   }
   notification_channels = [google_monitoring_notification_channel.email_alert.name]
@@ -31,6 +35,10 @@ resource "google_monitoring_alert_policy" "server_errors" {
       duration        = "60s"
       comparison      = "COMPARISON_GT"
       threshold_value = 5
+      aggregations {
+        alignment_period   = "60s"
+        per_series_aligner = "ALIGN_RATE"
+      }
     }
   }
   notification_channels = [google_monitoring_notification_channel.email_alert.name]
@@ -39,12 +47,12 @@ resource "google_monitoring_alert_policy" "server_errors" {
 resource "google_monitoring_dashboard" "lb_dashboard" {
   dashboard_json = <<EOF
 {
-  "displayName": "${var.prefix}-Security-and-Traffic-Dashboard",
+  "displayName": "${var.prefix}-Security-Dashboard",
   "gridLayout": {
     "columns": "2",
     "widgets": [
       {
-        "title": "Total HTTP/HTTPS Requests",
+        "title": "Total HTTPS Requests",
         "xyChart": {
           "dataSets": [{
             "timeSeriesQuery": {
@@ -56,12 +64,12 @@ resource "google_monitoring_dashboard" "lb_dashboard" {
         }
       },
       {
-        "title": "Cloud Armor Blocked Requests (403)",
+        "title": "Blocked Requests (403)",
         "xyChart": {
           "dataSets": [{
             "timeSeriesQuery": {
               "timeSeriesFilter": {
-                "filter": "metric.type=\"networksecurity.googleapis.com/edge_security_policy/request_count\" resource.type=\"edge_security_policy\" metric.labels.outcome=\"DENY\""
+                "filter": "metric.type=\"loadbalancing.googleapis.com/https/request_count\" resource.type=\"https_lb_rule\" metric.labels.response_code_class=\"400\""
               }
             }
           }]

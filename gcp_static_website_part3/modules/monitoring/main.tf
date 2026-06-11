57982315f4076e25641a782dadcf1447142ac134
sebastiancielma <cielmasebastian@gmail.com>
2026-06-11 10:10:11 +0200

    feat: add monitoring configuration

---
+++ gcp_static_website_part3/modules/monitoring/main.tf
@@ -0,0 +1,74 @@
+resource "google_monitoring_notification_channel" "email_alert" {
+  display_name = "${var.prefix}-devops-email"
+  type         = "email"
+  labels = {
+    email_address = var.alert_email
+  }
+}
+
+resource "google_monitoring_alert_policy" "armor_blocks" {
+  display_name = "${var.prefix}-high-armor-blocks"
+  combiner     = "OR"
+  conditions {
+    display_name = "Cloud Armor Deny > 10 in 1 min"
+    condition_threshold {
+      filter     = "metric.type=\"networksecurity.googleapis.com/edge_security_policy/request_count\" resource.type=\"edge_security_policy\" metric.labels.outcome=\"DENY\""
+      duration   = "60s"
+      comparison = "COMPARISON_GT"
+      threshold_value = 10
+    }
+  }
+  notification_channels = [google_monitoring_notification_channel.email_alert.name]
+}
+
+resource "google_monitoring_alert_policy" "server_errors" {
+  display_name = "${var.prefix}-high-5xx-errors"
+  combiner     = "OR"
+  conditions {
+    display_name = "5xx Errors > 5 in 1 min"
+    condition_threshold {
+      filter     = "metric.type=\"loadbalancing.googleapis.com/https/request_count\" resource.type=\"https_lb_rule\" metric.labels.response_code_class=\"500\""
+      duration   = "60s"
+      comparison = "COMPARISON_GT"
+      threshold_value = 5
+    }
+  }
+  notification_channels = [google_monitoring_notification_channel.email_alert.name]
+}
+
+resource "google_monitoring_dashboard" "lb_dashboard" {
+  dashboard_json = <<EOF
+{
+  "displayName": "${var.prefix}-Security-and-Traffic-Dashboard",
+  "gridLayout": {
+    "columns": "2",
+    "widgets": [
+      {
+        "title": "Total HTTP/HTTPS Requests",
+        "xyChart": {
+          "dataSets": [{
+            "timeSeriesQuery": {
+              "timeSeriesFilter": {
+                "filter": "metric.type=\"loadbalancing.googleapis.com/https/request_count\" resource.type=\"https_lb_rule\""
+              }
+            }
+          }]
+        }
+      },
+      {
+        "title": "Cloud Armor Blocked Requests (403)",
+        "xyChart": {
+          "dataSets": [{
+            "timeSeriesQuery": {
+              "timeSeriesFilter": {
+                "filter": "metric.type=\"networksecurity.googleapis.com/edge_security_policy/request_count\" resource.type=\"edge_security_policy\" metric.labels.outcome=\"DENY\""
+              }
+            }
+          }]
+        }
+      }
+    ]
+  }
+}
+EOF
+}
---
+++ gcp_static_website_part3/modules/monitoring/variables.tf
@@ -0,0 +1,9 @@
+variable "prefix" {
+  type        = string
+  description = "Prefix for naming resources"
+}
+
+variable "alert_email" {
+  type        = string
+  description = "Email address to send Cloud Monitoring alerts"
+}

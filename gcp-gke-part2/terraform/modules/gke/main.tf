5d94917264db89ee4c4c0ee9a1cccfd249d1706e
sebastiancielma <cielmasebastian@gmail.com>
2026-06-19 23:07:03 +0200

    feat: add gke configuration

---
+++ gcp-gke-part2/terraform/modules/gke/main.tf
@@ -0,0 +1,33 @@
+resource "google_service_account" "default" {
+  account_id   = "${var.cluster_name}-sa"
+  display_name = "GKE Minimal Service Account"
+}
+
+resource "google_container_cluster" "primary" {
+  name     = var.cluster_name
+  location = "${var.region}-a"
+
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  workload_identity_config {
+    workload_pool = "${var.project_id}.svc.id.goog"
+  }
+
+  resource_labels = var.tags
+}
+
+resource "google_container_node_pool" "primary_nodes" {
+  name       = "${var.cluster_name}-node-pool"
+  cluster    = google_container_cluster.primary.name
+  location   = google_container_cluster.primary.location
+  node_count = 1
+
+  node_config {
+    machine_type    = "e2-medium"
+    service_account = google_service_account.default.email
+    oauth_scopes    = ["https://www.googleapis.com/auth/cloud-platform"]
+    
+    resource_labels = var.tags
+  }
+}
---
+++ gcp-gke-part2/terraform/modules/gke/outputs.tf
@@ -0,0 +1,2 @@
+output "cluster_name" { value = google_container_cluster.primary.name }
+output "cluster_location" { value = google_container_cluster.primary.location }

eeb7266c448d50b30af337e7ccc3b8f3e39dfd72
sebastiancielma <cielmasebastian@gmail.com>
2026-06-22 21:39:58 +0200

    feat: add terraform root configuration

---
+++ gcp-gke-part3/terraform/main.tf
@@ -0,0 +1,28 @@
+module "network" {
+  source     = "./modules/network"
+  project_id = var.project_id
+  region     = var.region
+  vpc_name   = "private-gke-vpc"
+  tags       = var.tags
+}
+
+module "bastion" {
+  source       = "./modules/bastion"
+  project_id   = var.project_id
+  region       = var.region
+  network_name = module.network.vpc_name
+  subnet_name  = module.network.system_subnet_name
+  tags         = var.tags
+}
+
+module "gke" {
+  source             = "./modules/gke"
+  project_id         = var.project_id
+  region             = var.region
+  network_id         = module.network.vpc_id
+  subnet_id          = module.network.workload_subnet_name
+  pod_range_name     = module.network.pod_range_name
+  service_range_name = module.network.service_range_name
+  bastion_cidr       = "10.40.0.0/28" 
+  tags               = var.tags
+}

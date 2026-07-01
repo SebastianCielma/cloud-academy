2e77f44f79b5927984c51e92ac621f14402cb552
sebastiancielma <cielmasebastian@gmail.com>
2026-07-01 22:21:54 +0200

    feat: add terraform root files

---
+++ gcp-payment-app-part4/terraform/main.tf
@@ -0,0 +1,31 @@
+module "vpc" {
+  source       = "./modules/vpc"
+  project_id   = var.project_id
+  region       = var.region
+  network_name = "payment-vpc-prod"
+  subnet_name  = "payment-subnet-prod"
+}
+
+module "cloudsql" {
+  source     = "./modules/cloudsql"
+  project_id = var.project_id
+  region     = var.region
+  network_id = module.vpc.network_id
+}
+
+module "gke" {
+  source      = "./modules/gke"
+  project_id  = var.project_id
+  region      = var.region
+  network_id  = module.vpc.network_id
+  subnet_name = module.vpc.subnet_name
+}
+
+module "postgres_vm" {
+  source = "./modules/postgres-vm"
+
+  project_id   = var.project_id
+  region       = var.region
+  network_name = "payment-vpc-prod"    
+  subnet_name  = "payment-subnet-prod" 
+}
---
+++ gcp-payment-app-part4/terraform/outputs.tf
@@ -0,0 +1,20 @@
+output "gke_cluster_name" {
+  value = module.gke.cluster_name
+}
+
+output "db_private_ip" {
+  value = module.cloudsql.private_ip_address
+}
+
+output "db_user" {
+  value = module.cloudsql.db_user
+}
+
+output "db_password" {
+  value     = module.cloudsql.db_password
+  sensitive = true
+}
+
+output "vm_db_private_ip" {
+  value = module.postgres_vm.private_ip
+}
